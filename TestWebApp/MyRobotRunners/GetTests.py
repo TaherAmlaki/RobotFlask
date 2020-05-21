b462a5e85a58550627da8d7fb9a1574d10439988
TaherAmlaki <taher.amlaki@gmail.com>
2020-05-21 03:39:13 +0200

    Fix for GetTests test case index

--- TestWebApp/MyRobotRunners/GetTests.py
+++ TestWebApp/MyRobotRunners/GetTests.py
@@ -12,12 +12,9 @@ class GetSuiteInfo:
     SUITE_NAME = "SuiteName"
     SUITE_PATH = "SuitePath"
     SUITE_SHORT_PATH = "SuiteShortPath"
-    SUITE_DOC = "SuiteDoc"
-    SUITE_META_DATA = "SuiteMetaData"
     SUITE_VARIABLES = "SuiteVariables"
     SUITE_TEST_NUMBERS = "SuiteTestNumbers"
     SUITE_TESTS = "Tests"
-    SUITE_CHILDREN = "Children"
     TEST_NAME = "TestName"
     TEST_TAGS = "TestTags"
     TEST_DOCUMENTS = "TestDocs"
@@ -31,24 +28,18 @@ class GetSuiteInfo:
                   f.endswith(".robot")]
         for suite_path in suites:
             try:
-                suite = TestData(parent=None, source=suite_path)
+                suite_data = TestData(parent=None, source=suite_path)
             except NoTestsFound:
                 data = {GetSuiteInfo.SUITE_PATH: suite_path,
                         GetSuiteInfo.SUITE_SHORT_PATH: GetSuiteInfo.get_short_path(suite_path)}
             else:
-                metadata = []
-                if suite.setting_table.metadata:
-                    metadata = [{"MetaName": m.name, "MetaValue": m.value} for m in suite.setting_table.metadata.data]
-                variables = [{"VarName": v.name, "VarValue": v.value} for v in suite.variable_table.variables]
+                variables = [{"VarName": v.name, "VarValue": v.value} for v in suite_data.variable_table.variables]
                 tests, children = GetSuiteInfo.get_tests_for_suite(suite_path)
-                data = {GetSuiteInfo.SUITE_NAME: suite.name,
-                        GetSuiteInfo.SUITE_PATH: os.path.abspath(suite.source),
+                data = {GetSuiteInfo.SUITE_NAME: suite_data.name,
+                        GetSuiteInfo.SUITE_PATH: os.path.abspath(suite_data.source),
                         GetSuiteInfo.SUITE_SHORT_PATH: GetSuiteInfo.get_short_path(suite_path),
-                        GetSuiteInfo.SUITE_DOC: str(suite.setting_table.doc),
-                        GetSuiteInfo.SUITE_META_DATA: metadata,
                         GetSuiteInfo.SUITE_VARIABLES: variables,
                         GetSuiteInfo.SUITE_TESTS: tests,
-                        GetSuiteInfo.SUITE_CHILDREN: children,
                         GetSuiteInfo.SUITE_TEST_NUMBERS: len(tests) if tests else 0}
             if data.get(GetSuiteInfo.SUITE_NAME) is not None:
                 suites_data.append(data)
@@ -64,20 +55,22 @@ class GetSuiteInfo:
             list of all test cases in the suite and its children
         """
         try:
-            suite = TestData(parent=parent, source=path_to_suite)
+            tests_data = TestData(parent=parent, source=path_to_suite)
         except NoTestsFound:
             return [], []
         else:
             tests = []
-            for test_case in suite.testcase_table:
-                steps = getattr(suite.testcase_table.tests[0], "steps")
+            text_index = 0
+            for test_case in tests_data.testcase_table:
+                steps = getattr(tests_data.testcase_table.tests[text_index], "steps")
                 steps = [{'step': step.name, "args": step.args} for step in steps if getattr(step, "name", None)]
                 tests.append({GetSuiteInfo.TEST_NAME: test_case.name,
                               GetSuiteInfo.TEST_TAGS: test_case.tags.as_list()[1:],
                               GetSuiteInfo.TEST_DOCUMENTS: str(test_case.doc),
                               GetSuiteInfo.TEST_STEPS: steps,
                               GetSuiteInfo.TEST_ID: secrets.token_hex(nbytes=8)})
-            children = [child.name for child in suite.children]
+                text_index += 1
+            children = [child.name for child in tests_data.children]
             return tests, children
 
     @staticmethod
@@ -89,7 +82,7 @@ class GetSuiteInfo:
 
 if __name__ == "__main__":
     from pprint import pprint
-    GetSuiteInfo.TESTS_DIR = "../../Test suites"
+    GetSuiteInfo.TESTS_DIR = "../../Tests"
     suites_ = GetSuiteInfo.retrieve_all_tests()
     for suite_ in suites_:
         pprint(suite_)
